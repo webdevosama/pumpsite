ebebe6d6f5d629308809b0159e775a0d9657b003
osama <osama2002ibrahim@gmail.com>
2026-04-21 21:21:30 +0200

    moving towards local tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./**/*.html",                 // all HTML files in root and subfolders
+        "./components/**/*.html",      // your component partials (English)
+        "./ar/**/*.html",              // Arabic HTML files
+        "./ar/components/**/*.html",   // Arabic component partials
+        "./js/**/*.js",                // if you have Tailwind classes in JS
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'main': '#1e3575',
+                'secondary': '#3D90C6',
+                'bg-color': '#dee8ef',
+                'white': '#ffffff',
+            },
+            // any other custom extensions you have in variables.css
+        },
+    },
+    plugins: [],
+}
